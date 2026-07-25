256e2002686864a39e79a3547ccfc8af9d13b94e
Nova Storm <fearlesstechofficial@gmail.com>
2026-07-25 16:59:57 -0500

    Improve freecam update loop

--- client/main.lua
+++ client/main.lua
@@ -1,83 +1,70 @@
 local SETTINGS = _G.CONTROL_SETTINGS
 local CONTROLS = _G.CONTROL_MAPPING
 
--------------------------------------------------------------------------------
-
 local function GetSpeedMultiplier()
-  local fastNormal = GetSmartControlNormal(CONTROLS.MOVE_FAST)
-  local slowNormal = GetSmartControlNormal(CONTROLS.MOVE_SLOW)
+    local fastNormal = GetSmartControlNormal(CONTROLS.MOVE_FAST)
+    local slowNormal = GetSmartControlNormal(CONTROLS.MOVE_SLOW)
 
-  local baseSpeed = SETTINGS.BASE_MOVE_MULTIPLIER
-  local fastSpeed = 1 + ((SETTINGS.FAST_MOVE_MULTIPLIER - 1) * fastNormal)
-  local slowSpeed = 1 + ((SETTINGS.SLOW_MOVE_MULTIPLIER - 1) * slowNormal)
+    local baseSpeed = math.max(0.0, SETTINGS.BASE_MOVE_MULTIPLIER)
+    local fastSpeed = 1.0 + ((math.max(1.0, SETTINGS.FAST_MOVE_MULTIPLIER) - 1.0) * fastNormal)
+    local slowSpeed = 1.0 + ((math.max(1.0, SETTINGS.SLOW_MOVE_MULTIPLIER) - 1.0) * slowNormal)
+    local frameMultiplier = GetFrameTime() * 60.0
 
-  local frameMultiplier = GetFrameTime() * 60
-  local speedMultiplier = baseSpeed * fastSpeed / slowSpeed
-
-  return speedMultiplier * frameMultiplier
+    return (baseSpeed * fastSpeed / slowSpeed) * frameMultiplier
 end
 
 local function UpdateCamera()
-  if not IsFreecamActive() or IsPauseMenuActive() then
-    return
-  end
+    if IsPauseMenuActive() then
+        return
+    end
 
-  if not IsFreecamFrozen() then
-    local vecX, vecY = GetFreecamMatrix()
-    local vecZ = vector3(0, 0, 1)
+    if not IsFreecamFrozen() then
+        local vecX, vecY = GetFreecamMatrix()
+        local vecZ = vector3(0.0, 0.0, 1.0)
+        local pos = GetFreecamPosition()
+        local rot = GetFreecamRotation()
 
-    local pos = GetFreecamPosition()
-    local rot = GetFreecamRotation()
+        if not vecX or not vecY or not pos or not rot then
+            return
+        end
 
-    -- Get speed multiplier for movement
-    local speedMultiplier = GetSpeedMultiplier()
+        local speedMultiplier = GetSpeedMultiplier()
 
-    -- Get rotation input
-    local lookX = GetSmartControlNormal(CONTROLS.LOOK_X)
-    local lookY = GetSmartControlNormal(CONTROLS.LOOK_Y)
+        local lookX = GetSmartControlNormal(CONTROLS.LOOK_X)
+        local lookY = GetSmartControlNormal(CONTROLS.LOOK_Y)
+        local moveX = GetSmartControlNormal(CONTROLS.MOVE_X)
+        local moveY = GetSmartControlNormal(CONTROLS.MOVE_Y)
+        local moveZ = GetSmartControlNormal(CONTROLS.MOVE_Z)
 
-    -- Get position input
-    local moveX = GetSmartControlNormal(CONTROLS.MOVE_X)
-    local moveY = GetSmartControlNormal(CONTROLS.MOVE_Y)
-    local moveZ = GetSmartControlNormal(CONTROLS.MOVE_Z)
+        local rotX = rot.x + (-lookY * SETTINGS.LOOK_SENSITIVITY_X)
+        local rotZ = rot.z + (-lookX * SETTINGS.LOOK_SENSITIVITY_Y)
 
-    -- Calculate new rotation.
-    local rotX = rot.x + (-lookY * SETTINGS.LOOK_SENSITIVITY_X)
-    local rotZ = rot.z + (-lookX * SETTINGS.LOOK_SENSITIVITY_Y)
-    local rotY = rot.y
+        pos = pos + (vecX * moveX * speedMultiplier)
+        pos = pos + (vecY * -moveY * speedMultiplier)
+        pos = pos + (vecZ * moveZ * speedMultiplier)
 
-    -- Adjust position relative to camera rotation.
-    pos = pos + (vecX *  moveX * speedMultiplier)
-    pos = pos + (vecY * -moveY * speedMultiplier)
-    pos = pos + (vecZ *  moveZ * speedMultiplier)
+        SetFreecamPosition(pos)
+        SetFreecamRotation(rotX, rot.y, rotZ)
+    end
 
-    -- Adjust new rotation
-    rot = vector3(rotX, rotY, rotZ)
-
-    -- Update camera
-    SetFreecamPosition(pos.x, pos.y, pos.z)
-    SetFreecamRotation(rot.x, rot.y, rot.z)
-  end
-
-  -- Trigger a tick event. Resources depending on the freecam position can
-  -- make use of this event.
-  TriggerEvent('freecam:onTick')
+    TriggerEvent('freecam:onTick')
 end
 
--------------------------------------------------------------------------------
-
-Citizen.CreateThread(function ()
-  while true do
-    Citizen.Wait(0)
-    UpdateCamera()
-  end
+CreateThread(function()
+    while true do
+        if IsFreecamActive() then
+            Wait(0)
+            UpdateCamera()
+        else
+            Wait(250)
+        end
+    end
 end)
 
---------------------------------------------------------------------------------
+AddEventHandler('onResourceStop', function(resourceName)
+    if resourceName ~= GetCurrentResourceName() then
+        return
+    end
 
--- When the resource is stopped, make sure to return the camera to the player.
-AddEventHandler('onResourceStop', function (resourceName)
-  if resourceName == GetCurrentResourceName() then
     SetFreecamActive(false)
-  end
 end)
